6e44d2eeebdd5cf6b62c80644723665e3c116713
Christian <c@ctoverton.com>
2019-05-13 17:53:34 -0400

    popup progress

--- app/background/background.js
+++ app/background/background.js
@@ -2,16 +2,69 @@ let spotifyApi = {
     client_id: '7c5e232cdb464da0913baf19042cf106',
     client_secret: '0e1ce17da01b4034a92cab1ef28a8bdc',
     scopes: 'playlist-read-private, playlist-read-collaborative',
-}
+    login: {
+        auth: {
+            access_token: undefined,
+            refresh_token: undefined,
+            expiration: undefined,
+        },
+        display_name: 'Christian Overton',
+        id: '1251570824',
+        email: 'christianoverton@ctoverton.com',
+        avatar: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/16730162_759001374257510_2090258016787878205_n.jpg?_nc_cat=103&_nc_ht=scontent.xx&oh=71316afaca83541c0ddf77a3fe879ec2&oe=5D594311'
+    }
+};
 
 chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if (request.type == "api/getUserPlaylists"){
+    function (request, sender, sendResponse) {
+        if (request.action === 'checkLogin') {
+            checkLogin()
+                .then(function (result) {
+                    sendResponse(result);
+                });
+        }
+        if (request.action === 'getLogin') {
+            getLogin()
+                .then(function (result) {
+                        sendResponse(result);
+                    },
+                    function (err) {
+                        sendResponse(err);
+                    });
+        }
+        if (request.action === 'api/getUserPlaylists') {
             api.getUserPlaylists(sendResponse);
-            return true;
         }
         return true;
     });
+
+function checkLogin() {
+    return getLogin()
+        .then(function (result) {
+            return true;
+        }, function (err) {
+            return false;
+        })
+}
+
+function getLogin() {
+    return new Promise(function (resolve, reject) {
+        let key = 'login';
+        chrome.storage.sync.get([key], result => {
+            if (key in result) {
+                let login = result[key];
+                if ('access_token' in login.auth && 'refresh_token' in login.auth) {
+                    resolve(login);
+                } else {
+                    reject({error: 'No login auth found'})
+                }
+            } else {
+                reject({error: 'No login info'})
+            }
+        });
+    })
+}
+
 
 let api = {
     getUserPlaylists: function (callback) {
@@ -21,18 +74,23 @@ let api = {
             "url": "https://api.spotify.com/v1/users/1251570824/playlists?limit=50",
             "method": "GET",
             "headers": {
-                // "Authorization": "Bearer " + , Todo
+                "Authorization": "Bearer " + spotifyApi.login.auth.access_token,
                 "cache-control": "no-cache"
             }
         };
 
-        $.ajax(settings).done(function (response) {
-            console.log('api_getUserPlaylists: ', response);
-            if (!('error' in response)) {
+        $.ajax(settings)
+            .done(function (response) {
+                if (!('error' in response)) {
+                    callback(response);
+                } else if (response.error === 'The access token expired') {
+                    // todo refresh token
+                } else {
+                    callback(response);
+                }
+            })
+            .fail(function (response) {
                 callback(response);
-            } else if (response.error === 'The access token expired') {
-
-            }
-        });
+            });
     }
 };
--- app/popup/popup.js
+++ app/popup/popup.js
@@ -1,14 +1,20 @@
 let test_btn = $('#test_btn');
 
 test_btn.click(function () {
-    chrome.runtime.sendMessage({type: "test"}, function(response) {
+    /*console.log('btn clicked')
+    chrome.runtime.sendMessage({action: "api/getUserPlaylists"}, function(response) {
         console.log(response)
-    });
+    });*/
+    displayCurrentUser();
 });
 
 // --------------------------------------------------------------------------------
 
 let auth_btn = $('#auth_btn');
+let login_info = $('#login_info');
+let avatar = $('#avatar');
+let user_name = $('#user_name');
+let user_email = $('#user_email');
 
 auth_btn.click(function (e) {
     let btn = $(e.target);
@@ -19,33 +25,39 @@ auth_btn.click(function (e) {
     }
 });
 
-$(document).ready(function() {
-    checkLogin()
-        .then(function (result) {
-            console.log(result);
-            setAuthBtn('logout');
-        }, function (err) {
-            console.log(err);
-            setAuthBtn('login');
-        })
+login_info.click(function (e) {
+    chrome.tabs.create({url: $(this).attr('href')});
+    return false;
 });
 
-function checkLogin() {
-    return new Promise(function (resolve, reject) {
-        let key = 'login';
-        chrome.storage.sync.get([key], result => {
-            if (key in result) {
-                let login = result[key];
-                if ('access_token' in login.auth && 'refresh_token' in login.auth) {
-                    resolve(key.display_name + ' is logged in')
-                } else {
-                    reject('No login auth found')
-                }
-            } else {
-                reject('No login info')
-            }
-        });
-    })
+$(document).ready(function() {
+    // Check if user is logged in
+    background('checkLogin', function(response) {
+        if (!response) { // Not logged in
+            setAuthBtn('login');
+        } else { // Logged in
+            displayCurrentUser();
+            setAuthBtn('logout');
+        }
+    });
+});
+
+function background(action, responseCallback) {
+    chrome.runtime.sendMessage({action: action}, responseCallback);
+}
+
+function displayCurrentUser() {
+    background('getLogin', function(response) {
+        if ('error' in response) {
+            console.log('There was an error getting the current login', response)
+        } else {
+            avatar.attr('src', response.avatar)
+            user_name.text(response.display_name);
+            user_email.text(response.email);
+            login_info.attr('href', 'https://open.spotify.com/user/' + response.id)
+            login_info.show();
+        }
+    });
 }
 
 function setAuthBtn(type) {
@@ -77,25 +89,26 @@ function login() {
             },
             display_name: 'Christian Overton',
             id: '1251570824',
+            email: 'christianoverton@ctoverton.com',
             avatar: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/16730162_759001374257510_2090258016787878205_n.jpg?_nc_cat=103&_nc_ht=scontent.xx&oh=71316afaca83541c0ddf77a3fe879ec2&oe=5D594311'
         }
     }, function () {
-        checkLogin()
-            .then(function (result) {
-                console.log(result);
-                setAuthBtn('logout');
-            }, function (err) {
-                console.log(err);
+        // Check if user is logged in
+        background('checkLogin', function(response) {
+            if (!response) { // Not logged in
                 setAuthBtn('login');
-            })
+            } else { // Logged in
+                displayCurrentUser();
+                setAuthBtn('logout');
+            }
+        });
     });
-
-    //
 }
 
 function logout() {
     console.log('Logged Out');
     chrome.storage.sync.clear();
+    login_info.hide();
     setAuthBtn('login');
 }
 
