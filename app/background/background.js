919d8781037ac54183611c9ad53ed70b99bb68da
Christian <c@ctoverton.com>
2019-05-13 23:17:58 -0400

    Popup Login

--- app/background/background.js
+++ app/background/background.js
@@ -2,19 +2,9 @@ let spotifyApi = {
     client_id: '7c5e232cdb464da0913baf19042cf106',
     client_secret: '0e1ce17da01b4034a92cab1ef28a8bdc',
     scopes: 'playlist-read-private, playlist-read-collaborative',
-    login: {
-        auth: {
-            access_token: undefined,
-            refresh_token: undefined,
-            expiration: undefined,
-        },
-        display_name: 'Christian Overton',
-        id: '1251570824',
-        email: 'christianoverton@ctoverton.com',
-        avatar: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/16730162_759001374257510_2090258016787878205_n.jpg?_nc_cat=103&_nc_ht=scontent.xx&oh=71316afaca83541c0ddf77a3fe879ec2&oe=5D594311'
-    }
 };
 
+// Message Passing
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         if (request.action === 'checkLogin') {
@@ -32,17 +22,30 @@ chrome.runtime.onMessage.addListener(
                         sendResponse(err);
                     });
         }
+        if (request.action === 'launchAuthFlow') {
+            launchAuthFlow();
+        }
         if (request.action === 'api/getUserPlaylists') {
-            api.getUserPlaylists(sendResponse);
+            /*api.getUserPlaylists()
+                .then(function (result) {
+                    sendResponse(result)
+                }, function (err) {
+                    sendResponse(err)
+                });*/
         }
         return true;
     });
+
+function launchAuthFlow() {
+    api.getAuthCode();
+}
 
 function checkLogin() {
     return getLogin()
         .then(function (result) {
             return true;
         }, function (err) {
+            console.log('checkLogin Error: ', err);
             return false;
         })
 }
@@ -65,32 +68,209 @@ function getLogin() {
     })
 }
 
+function setLogin(auth) {
+    let login = {
+        auth: auth,
+    };
+
+    api.getCurrentUser(auth.access_token)
+        .then(function (result) {
+            Object.assign(login, result); // Merges user info into login object
+            chrome.storage.sync.set({login: login}, function () {
+                console.log('Login set to: ', login);
+                chrome.runtime.sendMessage({action: 'updateDisplay'});
+            });
+        }, function (err) {
+            console.log(err);
+        })
+}
+
+function checkToken() {
+    return new Promise(function (resolve, reject) {
+        getLogin()
+            .then(function (result) {
+                if (checkExpired(result.auth.expiration)) {
+                    // Token has expired, use refresh token
+                    resolve({refresh: true, auth: result.auth});
+                } else {
+                    //resolve('Expiration is set to ' + moment(result.auth.expiration).format("dddd, MMMM Do YYYY, h:mm:ss a"));
+                    // Current token is fine
+                    resolve({refresh: false, auth: result.auth});
+                }
+            }, function (err) {
+                reject(err)
+            })
+    });
+}
+
+function checkExpired(unixTimeVal) {
+    return unixTimeVal < moment().valueOf();
+}
 
 let api = {
-    getUserPlaylists: function (callback) {
-        let settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://api.spotify.com/v1/users/1251570824/playlists?limit=50",
-            "method": "GET",
-            "headers": {
-                "Authorization": "Bearer " + spotifyApi.login.auth.access_token,
-                "cache-control": "no-cache"
-            }
-        };
+    getAuthCode: function () {
+        return new Promise(function (resolve, reject) {
+            let client_id = spotifyApi.client_id;
+            let redirect_uri = encodeURIComponent(chrome.identity.getRedirectURL());
+            // let state = generateRandomString(16);
+            let scope = spotifyApi.scopes ? '&scope=' + encodeURIComponent(spotifyApi.scopes) : '';
+            let show_dialog = true;
 
-        $.ajax(settings)
-            .done(function (response) {
-                if (!('error' in response)) {
-                    callback(response);
-                } else if (response.error === 'The access token expired') {
-                    // todo refresh token
-                } else {
-                    callback(response);
+            let settings = {
+                url: 'https://accounts.spotify.com/authorize' +
+                    '?response_type=code' +
+                    '&client_id=' + client_id +
+                    scope +
+                    '&redirect_uri=' + redirect_uri +
+                    '&show_dialog=' + show_dialog
+                ,
+                interactive: true
+            };
+
+            // Launch UI for user to sign into Spotify
+            chrome.identity.launchWebAuthFlow(settings,
+                // Once user grants permission (or doesn't) this event is called
+                function (redirectUrl) {
+                    // console.log(redirectUrl);
+                    let url = new URL(redirectUrl);
+                    let code = url.searchParams.get("code"); // Authorization code used to get a token
+                    let error = url.searchParams.get("error");
+
+                    if (code) { 
+                        api.getToken(code)
+                            .then(function (result) {
+                                let token = result.access_token;
+                                let refresh_token = result.refresh_token;
+                            }, function (err) {
+                                console.log(err);
+                            })
+                    }
+                    else if (error) { reject(error) }
+                });
+        });
+    },
+    getToken: function (code) {
+        return new Promise(function (resolve, reject) {
+            if (code) { // If code supplied, get completely new token
+                let settings = {
+                    "async": true,
+                    // "crossDomain": true,
+                    "url": "https://accounts.spotify.com/api/token",
+                    "method": "Post",
+                    "headers": {
+                        "cache-control": "no-cache"
+                    },
+                    "data": {
+                        grant_type: 'authorization_code',
+                        code: code,
+                        redirect_uri: chrome.identity.getRedirectURL(),
+                        client_id: spotifyApi.client_id,
+                        client_secret: spotifyApi.client_secret
+                    }
+                };
+
+                $.ajax(settings)
+                    .done(function (response) {
+                        setLogin(response);
+                        resolve(response); // resolves object, not just token
+                        // token: response.access_token,
+                        // refresh_token: response.refresh_token
+                    })
+                    .fail(function (err) {
+                        reject({error: err});
+                    });
+            } else { // If no code supplied, check if current token is fine
+                checkToken()
+                    .then(function (result) {
+                        if (!(result.refresh)) { // If current access token exists and is not expired
+                            resolve({access_token: result.auth.access_token, msg: 'Token from sync storage'});
+                        } else {
+                            let refresh_token = result.auth.refresh_token;
+
+                            let settings = {
+                                "async": true,
+                                // "crossDomain": true,
+                                "url": "https://accounts.spotify.com/api/token",
+                                "method": "Post",
+                                "headers": {
+                                    "cache-control": "no-cache"
+                                },
+                                "data": {
+                                    grant_type: 'refresh_token',
+                                    refresh_token: refresh_token,
+                                    redirect_uri: chrome.identity.getRedirectURL(),
+                                    client_id: spotifyApi.client_id,
+                                    client_secret: spotifyApi.client_secret
+                                }
+                            };
+
+                            $.ajax(settings)
+                                .done(function (response) {
+                                    setLogin(response);
+                                    resolve(response);
+                                })
+                                .fail(function (err) {
+                                    reject({error: err});
+                                });
+                        }
+                    }, function (err) {
+                        reject({error: err}); // No login info and no code supplied
+                    });
+            }
+        });
+    },
+    getCurrentUser: function(access_token) {
+        return new Promise(function (resolve, reject) {
+            let settings = {
+                "async": true,
+                // "crossDomain": true,
+                "url": "https://api.spotify.com/v1/me",
+                "method": "Get",
+                "headers": {
+                    "Authorization": 'Bearer ' + access_token,
+                    "cache-control": "no-cache"
+                },
+                "json": "true"
+            };
+
+            $.ajax(settings)
+                .done(function (response) {
+                    // console.log('Response: ', response);
+                    resolve(response);
+                })
+                .fail(function (err) {
+                    reject(err);
+                });
+        });
+
+    },
+    /*getUserPlaylists: function () {
+        return new Promise(function (resolve, reject) {
+            let settings = {
+                "async": true,
+                "crossDomain": true,
+                "url": "https://api.spotify.com/v1/users/1251570824/playlists?limit=50",
+                "method": "GET",
+                "headers": {
+                    "Authorization": "Bearer " + spotifyApi.login.auth.access_token, // NOPE
+                    "cache-control": "no-cache"
                 }
-            })
-            .fail(function (response) {
-                callback(response);
-            });
-    }
+            };
+
+            $.ajax(settings)
+                .done(function (response) {
+                    if (!('error' in response)) {
+                        resolve(response);
+                    } else if (response.error === 'The access token expired') {
+                        // todo refresh token
+                    } else {
+                        reject(response);
+                    }
+                })
+                .fail(function (response) {
+                    reject(response);
+                });
+        });
+    },*/
+
 };
--- app/popup/popup.js
+++ app/popup/popup.js
@@ -1,3 +1,4 @@
+// ========== [ Testing ] ==========
 let test_btn = $('#test_btn');
 
 test_btn.click(function () {
@@ -8,14 +9,21 @@ test_btn.click(function () {
     displayCurrentUser();
 });
 
-// --------------------------------------------------------------------------------
+// ============================================================
 
+
+// ========== [ Globals ] ==========
 let auth_btn = $('#auth_btn');
 let login_info = $('#login_info');
 let avatar = $('#avatar');
 let user_name = $('#user_name');
 let user_email = $('#user_email');
 
+$(document).ready(function() {
+    updateDisplay();
+});
+
+// ========== [ UI ] ==========
 auth_btn.click(function (e) {
     let btn = $(e.target);
     if (btn.data( "type" ) === 'login') {
@@ -25,13 +33,22 @@ auth_btn.click(function (e) {
     }
 });
 
-login_info.click(function (e) {
+login_info.click(function () {
     chrome.tabs.create({url: $(this).attr('href')});
     return false;
 });
 
-$(document).ready(function() {
-    // Check if user is logged in
+// ========== [ Message Passing ] ==========
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.action === 'updateDisplay') {
+            updateDisplay();
+        }
+        return true;
+    });
+
+// ========== [ Functions ] ==========
+function updateDisplay() {
     background('checkLogin', function(response) {
         if (!response) { // Not logged in
             setAuthBtn('login');
@@ -40,10 +57,10 @@ $(document).ready(function() {
             setAuthBtn('logout');
         }
     });
-});
+}
 
 function background(action, responseCallback) {
-    chrome.runtime.sendMessage({action: action}, responseCallback);
+    chrome.runtime.sendMessage({action: action}, responseCallback ? responseCallback: undefined);
 }
 
 function displayCurrentUser() {
@@ -51,10 +68,10 @@ function displayCurrentUser() {
         if ('error' in response) {
             console.log('There was an error getting the current login', response)
         } else {
-            avatar.attr('src', response.avatar)
+            avatar.attr('src', response.images[0].url);
             user_name.text(response.display_name);
             user_email.text(response.email);
-            login_info.attr('href', 'https://open.spotify.com/user/' + response.id)
+            login_info.attr('href', 'https://open.spotify.com/user/' + response.id);
             login_info.show();
         }
     });
@@ -80,7 +97,7 @@ function login() {
     console.log('Logging in')
 
     // FAKE LOGIN
-    chrome.storage.sync.set({
+    /*chrome.storage.sync.set({
         login: {
             auth: {
                 access_token: 'dsfgdfgdfgundefdfgdfgdfgined',
@@ -93,16 +110,11 @@ function login() {
             avatar: 'https://scontent.xx.fbcdn.net/v/t1.0-1/p200x200/16730162_759001374257510_2090258016787878205_n.jpg?_nc_cat=103&_nc_ht=scontent.xx&oh=71316afaca83541c0ddf77a3fe879ec2&oe=5D594311'
         }
     }, function () {
-        // Check if user is logged in
-        background('checkLogin', function(response) {
-            if (!response) { // Not logged in
-                setAuthBtn('login');
-            } else { // Logged in
-                displayCurrentUser();
-                setAuthBtn('logout');
-            }
-        });
-    });
+        updateDisplay()
+    });*/
+
+    // REAL LOGIN
+    background('launchAuthFlow')
 }
 
 function logout() {
